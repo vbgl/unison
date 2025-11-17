a31a8be155fbab90473f90ff56acda051d783877
Vincent Laporte <Vincent.Laporte@inria.fr>
2025-11-17 16:21:29 +0100

    Whitespace

--- src/unison/src/MachineIR/Util.hs
+++ src/unison/src/MachineIR/Util.hs
@@ -63,11 +63,11 @@ maxTempId MachineBundle {mbInstrs = mis} = maxZ (map maxTempId mis)
 
 maxZ = maybeMax 0
 
-traverseMachineFunction fun acc mf @ MachineFunction {mfBlocks = blocks} =
+traverseMachineFunction fun acc mf@MachineFunction {mfBlocks = blocks} =
   let (blocks', acc') = foldl (traverseMachineBlock fun) ([], acc) blocks
   in (mf {mfBlocks = blocks'}, acc')
 
-traverseMachineBlock fun (accCode, acc) b @ MachineBlock {mbInstructions = is} =
+traverseMachineBlock fun (accCode, acc) b@MachineBlock {mbInstructions = is} =
   let (is', acc') = fun ([], acc) is
   in (accCode ++ [b {mbInstructions = is'}], acc')
 
@@ -80,48 +80,48 @@ mapToTargetMachineInstruction f =
 concatMapToMachineInstruction =
     mapToMachineBlock . concatMapToMachineInstructionBlock
 
-concatMapToMachineInstructionBlock f mb @ MachineBlock {mbInstructions = mis} =
+concatMapToMachineInstructionBlock f mb@MachineBlock {mbInstructions = mis} =
   mb {mbInstructions = concatMap (concatMapToMachineSingle f) mis}
 
-concatMapToMachineSingle f mi @ MachineSingle {} =
+concatMapToMachineSingle f mi@MachineSingle {} =
     case f mi of
       [mi'] -> [mi']
       mis   -> [mkMachineBundle mis]
 
-concatMapToMachineSingle f mi @ MachineBundle {mbInstrs = mis} =
+concatMapToMachineSingle f mi@MachineBundle {mbInstrs = mis} =
   [mi {mbInstrs = concatMap (concatMapToBundledMachineSingle f) mis}]
 
-concatMapToBundledMachineSingle f mi @ MachineSingle {} = f mi
+concatMapToBundledMachineSingle f mi@MachineSingle {} = f mi
 
 mapToMachineOperand f mi =
     fromSingleton $ concatMapToMachineSingle (mapToMachineSingleOperand f) mi
 
-mapToMachineSingleOperand f mi @ MachineSingle {msOperands = mos} =
+mapToMachineSingleOperand f mi@MachineSingle {msOperands = mos} =
     [mi {msOperands = map f mos}]
 
-mapToMachineBlock f mf @ MachineFunction {mfBlocks = mbs} =
+mapToMachineBlock f mf@MachineFunction {mfBlocks = mbs} =
   mf {mfBlocks = map f mbs}
 
 mapToMachineBlockId p f
-  mf @ MachineFunction {mfBlocks = mbs, mfProperties = mps} =
+  mf@MachineFunction {mfBlocks = mbs, mfProperties = mps} =
     mf {mfBlocks     = map (mapToMachineBlockIdBlock p f) mbs,
         mfProperties = map (mapToMachineBlockIdProperty f) mps}
 
-mapToMachineBlockIdBlock p f mb @ MachineBlock {mbId = id, mbInstructions = mis} =
+mapToMachineBlockIdBlock p f mb@MachineBlock {mbId = id, mbInstructions = mis} =
   let mis' = map (mapToMachineBlockIdInstr p f) mis
   in mb {mbId = f id, mbInstructions = mis'}
 
-mapToMachineBlockIdInstr p f mi @ MachineBundle {mbInstrs = mis} =
+mapToMachineBlockIdInstr p f mi@MachineBundle {mbInstrs = mis} =
   mi {mbInstrs = map (mapToMachineBlockIdInstr p f) mis}
 
-mapToMachineBlockIdInstr p f mi @ MachineSingle {msOperands = mos,
+mapToMachineBlockIdInstr p f mi@MachineSingle {msOperands = mos,
                                                  msProperties = mps}
   | p mi      = mi {msOperands   = map (mapToMachineBlockIdOperand f) mos,
                     msProperties = mapIf isMachineInstructionPropertyJTIBlocks
                                    (mapToMachineBlockIdPs f) mps}
   | otherwise = mi
 
-mapToMachineBlockIdOperand f mo @ MachineBlockRef {mbrId = id} =
+mapToMachineBlockIdOperand f mo@MachineBlockRef {mbrId = id} =
   mo {mbrId = f id}
 mapToMachineBlockIdOperand _ mo = mo
 
@@ -130,11 +130,11 @@ mapToMachineBlockIdPs f (MachineInstructionPropertyJTIBlocks bs) =
 mapToMachineBlockIdPs _ p = p
 
 mapToMachineBlockIdProperty f
-  (jt @ MachineFunctionPropertyJumpTable {mfPropertyJumpTable = es}) =
+  (jt@MachineFunctionPropertyJumpTable {mfPropertyJumpTable = es}) =
       jt {mfPropertyJumpTable = map (mapToMachineBlockIdJTE f) es}
 mapToMachineBlockIdProperty _ mbp = mbp
 
-mapToMachineBlockIdJTE f e @ MachineJumpTableEntry {mjtBlocks = bs} =
+mapToMachineBlockIdJTE f e@MachineJumpTableEntry {mjtBlocks = bs} =
   e {mjtBlocks = map (mapToMachineBlockIdOperand f) bs}
 
 machineBlockFreq =
@@ -142,31 +142,31 @@ machineBlockFreq =
 
 filterMachineInstructions = mapToMachineBlock . filterMachineInstructionsBlock
 
-filterMachineInstructionsBlock f mb @ MachineBlock {mbInstructions = mis} =
+filterMachineInstructionsBlock f mb@MachineBlock {mbInstructions = mis} =
   mb {mbInstructions = concatMap (filterInstruction f) mis}
 
-filterInstruction f mb @ MachineBundle {mbInstrs = mis} =
+filterInstruction f mb@MachineBundle {mbInstrs = mis} =
   case filter f mis of
      []   -> []
      mis' -> [mb {mbInstrs = mis'}]
-filterInstruction f ms @ MachineSingle {} = if f ms then [ms] else []
+filterInstruction f ms@MachineSingle {} = if f ms then [ms] else []
 
 runMachineTransformations ts mf = foldl (\mf f -> f mf) mf ts
 
-expandBlockPseudos f mi @ MachineBlock {mbInstructions = mis} =
+expandBlockPseudos f mi@MachineBlock {mbInstructions = mis} =
     let mis'   = map miToList mis
         mis''  = expand f mis'
         mis''' = map listToMi mis''
     in mi {mbInstructions = mis'''}
 
 miToList MachineBundle {mbInstrs = mis} = mis
-miToList mi @ MachineSingle {}          = [mi]
+miToList mi@MachineSingle {}          = [mi]
 
 listToMi [mi] = mi
 listToMi mis  = mkMachineBundle mis
 
 fallthroughBlock (MachineBlock {mbId = i1} :
-             b2 @ MachineBlock {mbId = i2} : bs) =
+             b2@MachineBlock {mbId = i2} : bs) =
                   (i1, i2) : fallthroughBlock (b2 : bs)
 fallthroughBlock [MachineBlock {mbId = i1}] = [(i1, -1)]
 
